cd4458135f7f8aa587b17784735b5958cedde945
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-03-05 15:23:28 +0000

    fix: add meta

--- src/new/meta.ts
+++ src/new/meta.ts
@@ -1,9 +1,47 @@
 export default [
     {
-        name: 'Elements that are illegal in a specific context',
-        scope: ['invalid.illegal'],
+        name: 'META - The complete contents of data structures',
+        scope: [
+            'meta.class',
+            'meta.struct',
+            'meta.enum',
+            'meta.union',
+            'meta.trait',
+            'meta.interface',
+            'meta.impl',
+            'meta.type',
+        ],
         settings: {
-            foreground: 'var(invalid)',
+            foreground: 'var(purple)',
+        },
+    },
+    {
+        name: 'META - The entire scope of a function',
+        scope: [
+            'meta.function',
+            'meta.function.parameters',
+            'meta.function.return-type',
+        ],
+        settings: {
+            foreground: 'var(purple)',
+        },
+    },
+    {
+        name: 'META - The entirety of a namespace',
+        scope: [
+            'meta.namespace',
+        ],
+        settings: {
+            foreground: 'var(blue)',
+        },
+    },
+    {
+        name: 'META - Preprocessor statements in language such as C',
+        scope: [
+            'meta.preprocessor',
+        ],
+        settings: {
+            foreground: 'var(cyan)',
         },
     },
 ];
