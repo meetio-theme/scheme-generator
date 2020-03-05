9d413cb371db9c2b05f7b5921024902928d4f8ff
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-03-05 15:37:53 +0000

    fix: meta

--- src/new/meta.ts
+++ src/new/meta.ts
@@ -28,20 +28,109 @@ export default [
     },
     {
         name: 'META - The entirety of a namespace',
-        scope: [
-            'meta.namespace',
-        ],
+        scope: ['meta.namespace'],
         settings: {
             foreground: 'var(blue)',
         },
     },
     {
         name: 'META - Preprocessor statements in language such as C',
-        scope: [
-            'meta.preprocessor',
-        ],
+        scope: ['meta.preprocessor'],
         settings: {
             foreground: 'var(cyan)',
+        },
+    },
+    {
+        name:
+            'META - Annotations, attributes and decorator statements that are used to modify the behavior or implementation of a class, method or function',
+        scope: [
+            'meta.annotation',
+            'meta.annotation.identifier',
+            'meta.annotation.parameters',
+            'punctuation.definition.annotation',
+        ],
+        settings: {
+            foreground: 'var(green)',
+        },
+    },
+    {
+        name: 'META - Complete identifiers, including namespace names',
+        scope: ['meta.path'],
+        settings: {
+            foreground: 'var(cyan)',
+        },
+    },
+    {
+        name:
+            'META - Function names, including the full path, and all parameters',
+        scope: ['meta.function-call'],
+        settings: {
+            foreground: 'var(cyan)',
+        },
+    },
+    {
+        name: 'META - Sections of code delineated by curly braces',
+        scope: [
+            'meta.block',
+            'punctuation.section.block.begin',
+            'punctuation.section.block.end',
+            'meta.braces',
+            'punctuation.section.braces.begin',
+            'punctuation.section.braces.end',
+        ],
+        settings: {
+            foreground: 'var(yellow)',
+        },
+    },
+    {
+        name: 'META - Sections of code delineated by parentheses',
+        scope: [
+            'meta.group',
+            'punctuation.section.group.begin',
+            'punctuation.section.group.end',
+            'meta.parens',
+            'punctuation.section.parens.begin',
+            'punctuation.section.parens.end',
+        ],
+        settings: {
+            foreground: 'var(yellow)',
+        },
+    },
+    {
+        name: 'META - Sections of code delineated by square brackets',
+        scope: [
+            'meta.brackets',
+            'punctuation.section.brackets.begin',
+            'punctuation.section.brackets.end',
+        ],
+        settings: {
+            foreground: 'var(yellow)',
+        },
+    },
+    {
+        name: 'META - Generic data type constructs',
+        scope: [
+            'meta.generic',
+            'punctuation.definition.generic.begin',
+            'punctuation.definition.generic.end',
+        ],
+        settings: {
+            foreground: 'var(yellow)',
+        },
+    },
+    {
+        name:
+            'META - HTML and XML tags, including punctuation, names and attributes',
+        scope: ['meta.tag'],
+        settings: {
+            foreground: 'var(red)',
+        },
+    },
+    {
+        name: 'META - Paragraphs in markup',
+        scope: ['meta.paragraph'],
+        settings: {
+            foreground: 'var(orange)',
         },
     },
 ];
