1cb12ae2d3a254fc135bb732785526e1c3459445
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-03-06 16:18:13 +0000

    fix: color

--- src/specific/diff.ts
+++ src/specific/diff.ts
@@ -3,32 +3,28 @@ export default [
         name: 'DIFF - Deleted',
         scope: ['diff.deleted'],
         settings: {
-            background: 'color(var(red) alpha(0.20))',
-            foreground_adjust: 'l(+ 5%)',
+            background: 'color(var(red) alpha(0.10))'
         },
     },
     {
         name: 'DIFF - Deleted Char',
         scope: ['diff.deleted.char'],
         settings: {
-            background: 'color(var(red) alpha(0.20))',
-            foreground_adjust: 'l(+ 10%)',
+            background: 'color(var(red) alpha(0.10))'
         },
     },
     {
         name: 'DIFF - Inserted',
         scope: ['diff.inserted'],
         settings: {
-            background: 'color(var(green) alpha(0.20))',
-            foreground_adjust: 'l(+ 5%)',
+            background: 'color(var(green) alpha(0.10))'
         },
     },
     {
         name: 'DIFF - Inserted Char',
         scope: ['diff.inserted.char'],
         settings: {
-            background: 'color(var(green) alpha(0.20))',
-            foreground_adjust: 'l(+ 10%)',
+            background: 'color(var(green) alpha(0.10))'
         },
     },
 ];
