e6b3955461409c193b16dfbda5b595149c7c3547
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-05-28 14:59:14 +0100

    fix: types

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -62,22 +62,23 @@ export interface IUi {
 }
 
 export interface IColors {
-    cursor: string;
-    foreground: string;
-    background: string;
-    comments: string;
-    blue: string;
-    brown: string;
-    cyan: string;
-    green: string;
-    orange: string;
-    pink: string;
-    purple: string;
-    red: string;
-    yellow: string;
-    invalid: string;
-    deprecated: string;
-    diffAdded: string;
-    diffModified: string;
-    diffDeleted: string;
+    cursor?: string;
+    foreground?: string;
+    background?: string;
+    comments?: string;
+    blue?: string;
+    brown?: string;
+    cyan?: string;
+    green?: string;
+    orange?: string;
+    pink?: string;
+    purple?: string;
+    red?: string;
+    gray?: string;
+    yellow?: string;
+    invalid?: string;
+    deprecated?: string;
+    diffAdded?: string;
+    diffModified?: string;
+    diffDeleted?: string;
 }
