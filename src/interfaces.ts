206c22d17342aca368155025d6f431634f9239e2
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-05-31 01:31:53 +0100

    feat: add new types

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,6 +4,8 @@ export interface IRules {
     settings: {
         foreground?: string;
         background?: string;
+        foreground_adjust?: string;
+        selection_foreground?: string;
         font_style?: string;
     };
 }
@@ -16,49 +18,96 @@ export interface ISchemeSetting {
 }
 
 export interface IUi {
-    accent?: string;
-    active_guide?: string;
+    /** The default background color. */
     background?: string;
-    block_caret?: string;
-    bracket_contents_foreground?: string;
-    bracket_contents_options?: string;
-    brackets_foreground?: string;
-    brackets_options?: string;
-    caret?: string;
-    find_highlight?: string;
-    find_highlight_foreground?: string;
-    fold_marker?: string;
+    /** The default color for text. */
     foreground?: string;
-    guide?: string;
-    gutter?: string;
-    gutter_foreground?: string;
-    highlight?: string;
-    inactive_selection?: string;
-    inactive_selection_border?: string;
-    inactive_selection_foreground?: string;
+    /** The color for whitespace, when rendered. */
     invisibles?: string;
-    line_diff_added?: string;
-    line_diff_deleted?: string;
-    line_diff_modified?: string;
-    line_diff_width?: string;
+    /** The color of the caret. */
+    caret?: string;
+    /** The color of the caret when using a block caret. (+3190) */
+    block_caret?: string;
+    /** The background color of the line containing the caret. */
     line_highlight?: string;
-    minimap_border?: string;
+    /** The color to use for the squiggly underline drawn under misspelled words. */
     misspelling?: string;
-    phantom_css?: string;
+    /** The color to use for the marker that indicates content has been folded. */
+    fold_marker?: string;
+    /** The color of the border drawn around the viewport area of the minimap.  */
+    minimap_border?: string;
+    /** The color search result positions drawn on top of the scroll bar. */
+    scroll_highlight?: string;
+    /** The color of the selected search result position drawn on top of the scroll bar. (+4050) */
+    scroll_selected_highlight?: string;
+    /** A color made available for use by the theme. (+4050) */
+    accent?: string;
+    /** CSS passed to popups. */
     popup_css?: string;
-    rulers?: string;
-    selection?: string;
-    selection_border?: string;
-    selection_border_width?: string;
-    selection_corner_radius?: string;
-    selection_corner_style?: string;
-    selection_foreground?: string;
-    shadow?: string;
-    shadow_width?: string;
+    /** CSS passed to phantoms. */
+    phantom_css?: string;
+    /** CSS passed to HTML sheets. (+4065) */
     sheet_css?: string;
+    /** The background color of the gutter. */
+    gutter?: string;
+    /** The color of line numbers in the gutter. */
+    gutter_foreground?: string;
+    /** The color of line numbers in the gutter when a line is highlighted. (+4050) */
+    gutter_foreground_highlight?: string;
+    /** The width of the diff lines, between 1 and 8. (+3186) */
+    line_diff_width?: number;
+    /** The color of diff markers for added lines. (+3186) */
+    line_diff_added?: string;
+    /** The color of diff markers for modified lines. (+3186) */
+    line_diff_modified?: string;
+    /** The color of diff markers for deleted lines. (+3186) */
+    line_diff_deleted?: string;
+    /** The background color of selected text. */
+    selection?: string;
+    /** A color that will override the scope-based text color of the selection. */
+    selection_foreground?: string;
+    /** The color for the border of the selection. */
+    selection_border?: string;
+    /** The width of the selection border, from 0 to 4. */
+    selection_border_width?: number;
+    /** The background color of a selection in a view that is not currently focused. */
+    inactive_selection?: string;
+    /** The color for the border of the selection in a view that is not currently focused. (+4074) */
+    inactive_selection_border?: string;
+    /** A color that will override the scope-based text color of the selection in a view that is not currently focused. */
+    inactive_selection_foreground?: string;
+    /** The style of corners to use on selections. Options include: 'round' (the default), 'cut' or 'square'. */
+    selection_corner_style?: 'round' | 'cut' | 'square';
+    /** The radius to use when the selection_corner_style. */
+    selection_corner_radius?: 'round' | 'cut';
+    /** The border color for "other" matches when the Highlight matches option is selected in the Find panel. */
+    highlight?: string;
+    /** The background color of text matched by the Find panel. */
+    find_highlight?: string;
+    /** A color that will override the scope-based text color of text matched by the Find panel. */
+    find_highlight_foreground?: string;
+    /** The color used to draw indent guides. */
+    guide?: string;
+    /** The color used to draw the indent guides for the indentation levels containing the caret. */
     stack_guide?: string;
+    /** The color used to draw the indent guides for the parent indentation levels of the indentation level containing the caret. */
+    active_guide?: string;
+    /** How brackets are highlighted when the caret is next to one. */
+    brackets_options?: 'underline' | 'stippled_underline' | 'squiggly_underline' | 'foreground' | 'bold' | 'italic';
+    /** The color to use when drawing the style specified by `brackets_options`. */
+    brackets_foreground?: string;
+    /** How brackets are highlighted when the caret is positioned in between a pair of brackets. */
+    bracket_contents_options?: 'underline' | 'stippled_underline' | 'squiggly_underline' | 'foreground';
+    /** The color to use when drawing the style specified by `brackets_contents_options`. */
+    bracket_contents_foreground?: string;
+    /** How tags are highlighted when the caret is inside of one. */
+    tags_options?: 'underline' | 'stippled_underline' | 'squiggly_underline' | 'foreground';
+    /** he color to use when drawing the style specified by tags_options. */
     tags_foreground?: string;
-    tags_options?: string;
+    /** The color of the shadow used to show when a text area can be horizontally scrolled. */
+    shadow?: string;
+    /** The width of the shadow in device-independent pixels. */
+    shadow_width?: string;
 }
 
 export interface IColors {
