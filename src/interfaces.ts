d7c6b9ff4a6a4ca612a0d941da9f6acc8fbff51f
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-06-02 16:34:32 +0100

    fix: update interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -115,6 +115,7 @@ export interface IColors {
     foreground: string;
     background: string;
     comments: string;
+    accent?: string;
     white?: string;
     black?: string;
     blue?: string;
