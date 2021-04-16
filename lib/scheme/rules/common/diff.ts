3730d8c1455b137041b92aac2980a59ced1cbd68
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2021-04-16 21:12:31 +0100

    fix: alpha value

--- lib/scheme/rules/common/diff.ts
+++ lib/scheme/rules/common/diff.ts
@@ -3,28 +3,28 @@ export default [
         name: 'DIFF - Deleted',
         scope: ['diff.deleted'],
         settings: {
-            background: 'color(var(diffDeleted) alpha(0.25))'
+            background: 'color(var(diffDeleted) alpha(0.20))'
         },
     },
     {
         name: 'DIFF - Deleted Char',
         scope: ['diff.deleted.char'],
         settings: {
-            background: 'color(var(diffDeleted) alpha(0.25))'
+            background: 'color(var(diffDeleted) alpha(0.20))'
         },
     },
     {
         name: 'DIFF - Inserted',
         scope: ['diff.inserted'],
         settings: {
-            background: 'color(var(diffAdded) alpha(0.25))'
+            background: 'color(var(diffAdded) alpha(0.20))'
         },
     },
     {
         name: 'DIFF - Inserted Char',
         scope: ['diff.inserted.char'],
         settings: {
-            background: 'color(var(diffAdded) alpha(0.25))'
+            background: 'color(var(diffAdded) alpha(0.20))'
         },
     },
 ];
